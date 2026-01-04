acb5a7e59a6b46f7889500df073e16015a712335
RakshithNM <bellare545@gmail.com>
2026-01-04 19:25:41 +0530

    Performance optimization

--- nvim/2025/init.lua
+++ nvim/2025/init.lua
@@ -1,3 +1,12 @@
+-- Performance: Enable Lua bytecode caching for faster startup
+vim.loader.enable()
+
+-- Disable optional providers to reduce warnings (uncomment if you don't need them)
+-- vim.g.loaded_node_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+
 -- Options
 vim.o.winborder = "rounded"
 vim.o.tabstop = 2
@@ -31,130 +40,175 @@ vim.opt.clipboard = "unnamedplus"
 local map = vim.keymap.set
 -- Source current file (useful for config files)
 map('n', '<leader>s', function()
-  vim.cmd.update()
-  vim.cmd.source('%')
+  vim.cmd('update | source %')
 end, { desc = "Save and source current file" })
 map('n', '<leader>w', ':write<CR>', { desc = "Write file" })
 
 -- Lazygit, Files, Grep and Help
 map('n', '<leader>gg', function()
 	vim.cmd('terminal lazygit')
-	vim.cmd('startinsert')
+	-- Ensure terminal buffer gets focus and enters insert mode
+	vim.schedule(function()
+		vim.cmd('startinsert')
+	end)
 end, { desc = "Open lazygit in terminal" })
 
+-- Essential plugins (loaded at startup)
 vim.pack.add({
 	{ src = "https://github.com/vague2k/vague.nvim" },
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
   { src = "https://github.com/nvim-lua/plenary.nvim" },
-  { src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim" },
   { src = "https://github.com/nvim-telescope/telescope.nvim" },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
-  { src = "https://github.com/RakshithNM/logdebug.nvim" },
-  { src = "https://github.com/m4xshen/hardtime.nvim" },
-  { src = "https://github.com/ellisonleao/glow.nvim" }
+  { src = "https://github.com/m4xshen/hardtime.nvim" }, -- Must load at startup to intercept keys
 })
 
--- Plugin configurations
--- Glow (markdown preview)
-local glow_ok, glow = pcall(require, "glow")
-if glow_ok then
-  glow.setup {
-    cmd = "Glow",
-    config = true
-  }
-end
+-- Optional plugins (lazy loaded)
+vim.pack.add({
+  { src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim", opt = true },
+  { src = "https://github.com/RakshithNM/logdebug.nvim", opt = true },
+  { src = "https://github.com/ellisonleao/glow.nvim", opt = true },
+})
 
--- Hardtime (vim practice)
-local hardtime_ok, hardtime = pcall(require, "hardtime")
-if hardtime_ok then
-  hardtime.setup {
-    max_time = 300,
-    max_count = 1
-  }
-end
+-- Plugin configurations (lazy loaded)
+-- Glow (markdown preview) - lazy load on markdown files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.cmd("packadd glow.nvim")
+    local glow_ok, glow = pcall(require, "glow")
+    if glow_ok then
+      glow.setup { cmd = "Glow", config = true }
+    end
+  end,
+  once = true,
+})
 
--- Logdebug
-local logdebug_ok, logdebug = pcall(require, "logdebug")
-if logdebug_ok then
-  logdebug.setup {
-  keymap_visual = "<leader>lv",
-  keymap_find = "<leader>fl",
-  filetypes = {
-    "javascript",
-    "typescript",
-    "javascriptreact",
-    "typescriptreact",
-    "vue",
-    "go",
-    "lua",
-    "ruby",
-    "python",
-  },
--- optional filetype filter
-  languages = {
-    go = {
-      build_log = function(indent, _level, expr)
-        return string.format('%slog.Printf("%%+v", %s)', indent, expr)
-      end,
-      is_log_line = function(line, _levels)
-        return line:match("^%s*log%.Printf%(") ~= nil
-      end,
-    },
-    lua = {
-      build_log = function(indent, _level, expr)
-        return string.format("%sprint(vim.inspect(%s))", indent, expr)
-      end,
-      is_log_line = function(line, _levels)
-        return line:match("^%s*print%(") ~= nil
-          or line:match("^%s*vim%.print%(") ~= nil
-      end,
-    },
-    ruby = {
-      build_log = function(indent, _level, expr)
-        return string.format("%sputs(%s.inspect)", indent, expr)
-      end,
-      is_log_line = function(line)
-        return line:match("^%s*puts%(") ~= nil
-      end,
-    },
-    python = {
-      build_log = function(indent, _level, expr)
-        return string.format("%sprint(%s)", indent, expr)
-      end,
-      is_log_line = function(line)
-        return line:match("^%s*print%(") ~= nil
-      end,
-    },
-  },
-  }
-end
+-- Hardtime (vim practice) - must load at startup to intercept keys
+vim.schedule(function()
+  local hardtime_ok, hardtime = pcall(require, "hardtime")
+  if hardtime_ok then
+    hardtime.setup {
+      max_time = 300,
+      max_count = 1,
+      disable_mouse = true,
+      hint = true,
+      notification = true,
+      allow_different_key = false,
+      restriction_mode = "block", -- "block" or "hint"
+      restricted_keys = {
+        ["h"] = { "n", "x" },
+        ["j"] = { "n", "x" },
+        ["k"] = { "n", "x" },
+        ["l"] = { "n", "x" },
+        ["+"] = { "n", "x" },
+        ["gj"] = { "n", "x" },
+        ["gk"] = { "n", "x" },
+      },
+      disabled_keys = {
+        ["<Up>"] = { "", "i" },
+        ["<Down>"] = { "", "i" },
+        ["<Left>"] = { "", "i" },
+        ["<Right>"] = { "", "i" },
+      },
+    }
+  end
+end)
 
--- Treesitter setup
-local treesitter_ok, treesitter = pcall(require, "nvim-treesitter")
-if treesitter_ok then
-  treesitter.setup {
-	ensure_installed = { 
-    "javascript", 
-    "typescript", 
-    "lua", 
-    "c", 
-    "cpp", 
-    "go", 
-    "html", 
-    "css", 
-    "json", 
-    "vim", 
-    "bash"
-  },
-	sync_install = false,
-	auto_install = false,
-	ignore_install = {},
-  indent = { enable = true },
-	highlight = { enable = true, additional_vim_regex_highlighting = false },
-	incremental_selection = { enable = true },
-	textobjects = { enable = false },
-  }
-end
+-- Logdebug - lazy load on supported filetypes
+local logdebug_loaded = false
+local logdebug_filetypes = {
+  "javascript", "typescript", "javascriptreact", "typescriptreact",
+  "vue", "go", "lua", "ruby", "python"
+}
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = table.concat(logdebug_filetypes, ","),
+  callback = function()
+    if not logdebug_loaded then
+      logdebug_loaded = true
+      vim.cmd("packadd logdebug.nvim")
+      local logdebug_ok, logdebug = pcall(require, "logdebug")
+      if logdebug_ok then
+        logdebug.setup {
+          keymap_visual = "<leader>lv",
+          keymap_find = "<leader>fl",
+          filetypes = logdebug_filetypes,
+          languages = {
+            go = {
+              build_log = function(indent, _level, expr)
+                return string.format('%slog.Printf("%%+v", %s)', indent, expr)
+              end,
+              is_log_line = function(line, _levels)
+                return line:match("^%s*log%.Printf%(") ~= nil
+              end,
+            },
+            lua = {
+              build_log = function(indent, _level, expr)
+                return string.format("%sprint(vim.inspect(%s))", indent, expr)
+              end,
+              is_log_line = function(line, _levels)
+                return line:match("^%s*print%(") ~= nil
+                  or line:match("^%s*vim%.print%(") ~= nil
+              end,
+            },
+            ruby = {
+              build_log = function(indent, _level, expr)
+                return string.format("%sputs(%s.inspect)", indent, expr)
+              end,
+              is_log_line = function(line)
+                return line:match("^%s*puts%(") ~= nil
+              end,
+            },
+            python = {
+              build_log = function(indent, _level, expr)
+                return string.format("%sprint(%s)", indent, expr)
+              end,
+              is_log_line = function(line)
+                return line:match("^%s*print%(") ~= nil
+              end,
+            },
+          },
+        }
+      end
+    end
+  end,
+  once = true,
+})
+
+-- Treesitter setup (deferred until first buffer)
+local treesitter_setup_done = false
+local treesitter_filetypes = {
+  javascript = true, typescript = true, lua = true, c = true, cpp = true,
+  go = true, html = true, css = true, json = true, vim = true, bash = true,
+  markdown = true, python = true, ruby = true, vue = true
+}
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    if not treesitter_setup_done then
+      local ft = vim.bo.filetype
+      if ft ~= "" and treesitter_filetypes[ft] then
+        treesitter_setup_done = true
+        local treesitter_ok, treesitter = pcall(require, "nvim-treesitter")
+        if treesitter_ok then
+          treesitter.setup {
+            ensure_installed = { 
+              "javascript", "typescript", "lua", "c", "cpp", "go",
+              "html", "css", "json", "vim", "bash"
+            },
+            sync_install = false,
+            auto_install = false,
+            ignore_install = {},
+            indent = { enable = true },
+            highlight = { enable = true, additional_vim_regex_highlighting = false },
+            incremental_selection = { enable = true },
+            textobjects = { enable = false },
+          }
+        end
+      end
+    end
+  end,
+  once = true,
+})
 
 -- LSP Configuration
 -- Enable LSP servers
@@ -217,28 +271,41 @@ map('n', '<leader>dl', function()
   vim.diagnostic.setloclist({ open = true })
 end, { desc = "Diagnostics → Loclist" })
 
--- Telescope setup
-local telescope_ok, telescope = pcall(require, 'telescope')
-if telescope_ok then
-  telescope.setup({
-    extensions = {
-      fzf = {
-        fuzzy = true,
-        override_generic_sorter = true,
-        override_file_sorter = true,
-        case_mode = "smart_case",
+-- Telescope setup (lazy loaded on first use)
+local telescope_initialized = false
+local function init_telescope()
+  if telescope_initialized then return end
+  telescope_initialized = true
+  local telescope_ok, telescope = pcall(require, 'telescope')
+  if telescope_ok then
+    telescope.setup({
+      extensions = {
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = "smart_case",
+        }
       }
-    }
-  })
-  -- Load fzf-native extension if available
-  pcall(telescope.load_extension, 'fzf')
+    })
+    -- Load fzf-native extension if available
+    pcall(telescope.load_extension, 'fzf')
+  end
 end
 
--- Telescope keymaps
-local builtin = require('telescope.builtin')
-map('n', '<leader><leader>', builtin.find_files, { desc = 'Telescope find files' })
-map('n', '<leader>/', builtin.live_grep, { desc = 'Telescope live grep' })
-map('n', '<leader>h', builtin.help_tags, { desc = 'Telescope help tags' })
+-- Telescope keymaps (lazy load on first use)
+map('n', '<leader><leader>', function()
+  init_telescope()
+  require('telescope.builtin').find_files()
+end, { desc = 'Telescope find files' })
+map('n', '<leader>/', function()
+  init_telescope()
+  require('telescope.builtin').live_grep()
+end, { desc = 'Telescope live grep' })
+map('n', '<leader>h', function()
+  init_telescope()
+  require('telescope.builtin').help_tags()
+end, { desc = 'Telescope help tags' })
 map('n', '<leader>a', 'ggVG', { desc = "Select all" })
 
 -- Completion in insert mode
@@ -266,13 +333,7 @@ vim.api.nvim_create_autocmd("FileType", {
     local function open_and_close()
       local info = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1]
       local is_loclist = info and info.loclist == 1
-      if is_loclist then
-        vim.cmd("ll")
-        vim.cmd("lclose")
-      else
-        vim.cmd("cc")
-        vim.cmd("cclose")
-      end
+      vim.cmd(is_loclist and "ll | lclose" or "cc | cclose")
     end
 
     vim.keymap.set("n", "<CR>", open_and_close, { buffer = ev.buf, silent = true })
@@ -283,14 +344,28 @@ map('n', '<leader>q', ':cclose<CR>', { silent = true, desc = 'Quickfix: close' }
 map('n', '<leader>cn',':cnext<CR>', { silent = true, desc = 'Quickfix: next' })
 map('n', '<leader>cp',':cprevious<CR>', { silent = true, desc = 'Quickfix: prev' })
 
--- Autowrite template file to new C files
+-- Template file paths (cached to avoid repeated expansions)
 local template_c = vim.fn.expand("~/.config/nvim/templates/c.c")
-if vim.fn.filereadable(template_c) == 1 then
+local template_cpp = vim.fn.expand("~/.config/nvim/templates/cpp.cc")
+local template_c_exists = vim.fn.filereadable(template_c) == 1
+local template_cpp_exists = vim.fn.filereadable(template_cpp) == 1
+
+-- Autowrite template file to new C files
+if template_c_exists then
   vim.api.nvim_create_autocmd("BufNewFile", {
     pattern = "*.c",
     command = "0r " .. template_c
   })
 end
+
+-- Autowrite template file to new CPP files
+if template_cpp_exists then
+  vim.api.nvim_create_autocmd("BufNewFile", {
+    pattern = "*.cc",
+    command = "0r " .. template_cpp
+  })
+end
+
 -- Compile C programs within vim
 map('n', '<F5>', function()
   vim.cmd.write()
@@ -298,20 +373,12 @@ map('n', '<F5>', function()
   local out  = vim.fn.expand('%:p:r')
   vim.cmd('!gcc -Wall -Wextra -O2 ' .. vim.fn.shellescape(file) .. ' -o ' .. vim.fn.shellescape(out))
 end, { desc = "Build C" })
+
 -- Run built binary
 map('n', '<F6>', function()
   local out = vim.fn.expand('%:p:r')
   vim.cmd('!' .. vim.fn.shellescape(out))
 end, { desc = "Run built binary" })
-
--- Autowrite template file to new CPP files
-local template_cpp = vim.fn.expand("~/.config/nvim/templates/cpp.cc")
-if vim.fn.filereadable(template_cpp) == 1 then
-  vim.api.nvim_create_autocmd("BufNewFile", {
-    pattern = "*.cc",
-    command = "0r " .. template_cpp
-  })
-end
 
 -- Netrw
 vim.g.netrw_banner = 0 -- No banner
@@ -327,10 +394,11 @@ vim.api.nvim_create_autocmd("FileType", {
   end
 })
 
--- Colorscheme
-local vague_ok, vague = pcall(require, "vague")
-if vague_ok then
-  vague.setup({ transparent = true })
-  vim.cmd("colorscheme vague")
-  vim.cmd(":hi statusline guibg=NONE")
-end
+-- Colorscheme (deferred to avoid blocking startup)
+vim.schedule(function()
+  local vague_ok, vague = pcall(require, "vague")
+  if vague_ok then
+    vague.setup({ transparent = true })
+    vim.cmd("colorscheme vague | hi statusline guibg=NONE")
+  end
+end)
